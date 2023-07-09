7f1e5531b329633a926a18ddca5448174a64ec01
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2023-07-09 05:19:28 +0000

    Pushka2

--- configuration.nix
+++ configuration.nix
@@ -8,16 +8,9 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./networking.nix
       ./users.nix
+      ./networking.nix
       ./packages.nix
-      ./netdata.nix
-      ./http.nix
-      ./postgresql.nix
-      ./matrix.nix
-      ./mail.nix
-      ./mumble.nix
-      ./gitea.nix
     ];
 
   # Use the GRUB 2 boot loader.
@@ -26,9 +19,9 @@
   # boot.loader.grub.efiInstallAsRemovable = true;
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/disk/by-id/wwn-0x5f8db4c041802907"; # or "nodev" for efi only
+  boot.loader.grub.device = "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0"; # or "nodev" for efi only
 
-  networking.hostName = "PUSHKA"; # Define your hostname.
+  # networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
@@ -51,9 +44,39 @@
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
 
+
+  
+
   # Configure keymap in X11
   # services.xserver.layout = "us";
   # services.xserver.xkbOptions = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.users.alice = {
+  #   isNormalUser = true;
+  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  #   packages = with pkgs; [
+  #     firefox
+  #     tree
+  #   ];
+  # };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # environment.systemPackages = with pkgs; [
+  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #   wget
+  # ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -69,25 +92,6 @@
   services.openssh.enable = true;
   services.openssh.settings.PasswordAuthentication = false;
   services.openssh.settings.PermitRootLogin = "no";
-
-  networking = {
-    defaultGateway = {
-      address = "198.204.240.193";
-      interface = "eth1";
-    };
-    # Use google's public DNS server
-    nameservers = [ "8.8.8.8" ];
-    interfaces.eth0.useDHCP = false;
-    interfaces.eth1 = {
-      useDHCP = false;
-      ipv4.addresses = [
-        {
-          address = "198.204.240.194";
-          prefixLength = 29;
-        }
-      ];
-    };
-  };
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -5,31 +5,21 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [ (modulesPath + "/profiles/qemu-guest.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "ahci" "ohci_pci" "ehci_pci" "pata_atiixp" "floppy" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/4f63ac8d-7379-46f8-a945-5861e31b4f94";
+    { device = "/dev/disk/by-uuid/51493f91-4308-4cce-9a0a-019aaa2ec586";
       fsType = "ext4";
     };
 
-  fileSystems."/mnt/hdd" =
-    { device = "/dev/disk/by-uuid/6632972d-0120-47c9-9784-7aba2b0643b5";
-      fsType = "btrfs";
-    };
-
-  fileSystems."/var/lib/matrix-synapse/media_store" =
-    { device = "/mnt/hdd/content/matrix-synapse/media_store";
-      options = [ "bind" ];
-    };
-
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/e0e52ef0-8b69-447b-b87d-b3625ebdf11c"; }
+    [ { device = "/dev/disk/by-uuid/3bebaa01-839c-45fc-aa38-990d04814589"; }
     ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
@@ -37,10 +27,8 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.eth0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.eth1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.ens18.useDHCP = lib.mkDefault true;
   # networking.interfaces.wg0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
